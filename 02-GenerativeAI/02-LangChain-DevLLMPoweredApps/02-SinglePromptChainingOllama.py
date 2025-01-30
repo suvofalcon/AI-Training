e7923c4a2453c4eafd88710ce6ebed6c3cb1a8c2
suvofalcon <suvo.bhattacharya@gmail.com>
2025-01-30 11:10:44 +0530

    Added StrOutputParser()

--- 02-GenerativeAI/02-LangChain-DevLLMPoweredApps/02-SinglePromptChainingOllama.py
+++ 02-GenerativeAI/02-LangChain-DevLLMPoweredApps/02-SinglePromptChainingOllama.py
@@ -15,7 +15,6 @@ of Pennsylvania, and received bachelor's degrees in economics and physics. He mo
 However, Musk dropped out after two days and, with his brother Kimbal, co-founded online city guide software company Zip2. The startup was 
 acquired by Compaq for $307 million in 1999, and, that same year Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 
 to form PayPal.In October 2002, eBay acquired PayPal for $1.5 billion, and that same year, with $100 million of the money he made, 
-Musk founded SpaceX, a spaceflight services company. In 2004, he became an early investor in electric vehicle manufacturer 
 Tesla Motors, Inc. (now Tesla, Inc.). He became its chairman and product architect, assuming the position of CEO in 2008. In 2006, Musk helped 
 create SolarCity, a solar-energy company that was acquired by Tesla in 2016 and became Tesla Energy. In 2013, he proposed a hyperloop 
 high-speed vactrain transportation system. In 2015, he co-founded OpenAI, a nonprofit artificial intelligence research company. 
