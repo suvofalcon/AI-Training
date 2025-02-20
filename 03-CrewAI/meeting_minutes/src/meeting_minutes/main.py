01bb513850a92ff6fc2f19491360908d87652015
suvofalcon <suvo.bhattacharya@gmail.com>
2025-02-20 19:31:07 +0530

    Meeting Minutes Crew - For taking a transcription and summarizing, generating action items and sentiment in a file and completing

---
+++ 03-CrewAI/meeting_minutes/src/meeting_minutes/main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from openai import OpenAI
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+from crewai.flow import Flow, listen, start
+from crews.meeting_minutes_crew.meeting_minutes_crew import MeetingMinutesCrew
+
+load_dotenv()
+
+class MeetingMinutesState(BaseModel):
+    transcript: str = ""
+    meeting_minutes: str = ""
+
+class MeetingMinutesFlow(Flow[MeetingMinutesState]):
+
+    @start()
+    def transcribe_meeting(self):
+        print("Generating Transcription")
+
+        # Load the audio file
+        audio = AudioSegment.from_file("EarningsCall.wav", format="wav")
+
+        # Define chunk length in milliseconds (e.g 1 minute = 60,000 ms)
+        chunk_length = 60000
+        chunks = make_chunks(audio, chunk_length)
+
+        # Transcribe each chunk
+        full_transcription = ""
+        for i, chunk in enumerate(chunks):
+            print(f"Transcribing Chunk {i+1} / {len(chunks)}")
+            chunk_path = f"chunk_{i}.wav"
+            chunk.export(chunk_path, format="wav")
+
+            with open(chunk_path, 'rb') as audio_file:
+                transcription = OpenAI().audio.transcriptions.create(
+                    model="whisper-1",
+                    file=audio_file
+                )
+                full_transcription += transcription.text + " "
+
+        self.state.transcript = full_transcription
+        print(f"Transcription Completed - {self.state.transcript}")
+
+
+    @listen(transcribe_meeting)
+    def generate_meeting_minutes(self):
+        print("Generating Meeting Minutes")
+
+        crew = MeetingMinutesCrew()
+        inputs = {
+            "transcript": self.state.transcript
+        }
+
+        meeting_minutes = crew.crew().kickoff(inputs=inputs)
+        self.state.meeting_minutes = meeting_minutes
+
+
+def kickoff():
+    meeting_minutes_flow = MeetingMinutesFlow()
+    meeting_minutes_flow.kickoff()
+
+
+def plot():
+    meeting_minutes_flow = MeetingMinutesFlow()
+    meeting_minutes_flow.plot()
+
+
+if __name__ == "__main__":
+    kickoff()
