dd64b097498eb98175a805a17fd3fc64521c2fe8
suvofalcon <suvo.bhattacharya@gmail.com>
2025-03-28 13:08:46 +0530

    Modified Web Search Agent by including image search and related news exploration

--- 06-Agno(PhiData)/agents/web_search_agent.py
+++ 06-Agno(PhiData)/agents/web_search_agent.py
@@ -15,6 +15,23 @@ def agent_search():
     )
     return web_agent
 
+def image_search_agent():
+    web_agent = Agent(
+        name='Image Search Agent',
+        role="Search the web for information related to images",
+        model=OpenAIChat(id="gpt-4o-mini"),
+        tools=[SerpApiTools(api_key=os.getenv("SERPAPI_API_KEY"))],
+        show_tool_calls=True,
+        markdown=True,
+    )
+    return web_agent
+
 if __name__ == "__main__":
     agent = agent_search()
-    agent.print_response("What's happening in France?", stream=True)
+    img_agent = image_search_agent()
+    agent.print_response("What's happening in France in 2025?", stream=True)
+    img_agent.print_response(
+        "Tell me about this image and give me the latest news about it.",
+        images=["https://upload.wikimedia.org/wikipedia/commons/b/bf/Krakow_-_Kosciol_Mariacki.jpg"],
+        stream=True,
+    )
